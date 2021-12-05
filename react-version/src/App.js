1313053800ef7f273ea76eebd28dc4223eb9139c
Conrad <conradwen@gmail.com>
2021-12-05 15:14:49 -0800

    copied css and edit layout

--- react-version/src/App.js
+++ react-version/src/App.js
@@ -9,8 +9,10 @@ function App() {
     <div className="App">
       <Navigation />
       <Profile />
-      <TweetList />
-      <TweetForm />
+      <main class="container">
+        <TweetList />
+        <TweetForm />
+      </main>
     </div>
   );
 }
